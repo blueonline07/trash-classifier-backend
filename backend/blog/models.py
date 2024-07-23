1b8cc4a74101a27d242c094af911ee14e08831c9
Khang Le Duy <khangleduy@Khangs-MacBook-Air.local>
2024-07-23 16:10:06 +0700

    models

---
+++ backend/blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class ContentModel(TimeStampedModel):
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    react = models.IntegerField(default=0)
+
+
+class Post (ContentModel):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+class Comment(ContentModel):
+    content = models.CharField(max_length=200)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Reaction(models.Model):
+    REACTION_CHOICES = [
+        (1, 'Like'),
+        (-1, 'Dislike'),
+    ]
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    content = models.ForeignKey(ContentModel, on_delete=models.CASCADE)
+    reaction_type = models.IntegerField(choices=REACTION_CHOICES)
