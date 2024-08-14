9a83f3a6048bc7fb8b5bba695439a29ae1bc41a2
Khang Le Duy <khangleduy@Khangs-MacBook-Air.local>
2024-08-14 17:18:13 +0700

    ok

--- backend/auths/urls.py
+++ backend/auths/urls.py
@@ -1,6 +1,6 @@
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from django.urls import path
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
