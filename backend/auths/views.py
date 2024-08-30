1193effc2d045c5ba6f3e01361795a2d4c81c0b3
Khang Le Duy <khangleduy@Khangs-MacBook-Air.local>
2024-08-30 15:37:29 +0700

    change google login

--- backend/auths/views.py
+++ backend/auths/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import GenericAPIView
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import TokenSerializer
+from .serializers import TokenSerializer, UserSerializer
 from django.contrib.auth.models import User
 from .models import VerificationCode
 from django.core.mail import send_mail
@@ -12,13 +12,40 @@ import requests
 
 # Create your views here.
 class GoogleLogin(GenericAPIView):
-    serializer_class = TokenSerializer
-    def post(self, request):
-        access_token = request.data.get('access_token')
-        data = requests.get(f"https://www.googleapis.com/oauth2/v2/userinfo", headers={
+    def get(self, request):
+        access_token = request.query_params.get('access_token')
+        resp = requests.get(f"https://www.googleapis.com/oauth2/v2/userinfo", headers={
             "Authorization": f"Bearer {access_token}"
         }).json()
-        user =  User.objects.get_or_create(username=data['email'])[0]
+        user = User.objects.filter(email=resp['email'])
+        if user:
+            refresh = RefreshToken.for_user(user)
+            data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
+            serializer = TokenSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            json = JSONRenderer().render(serializer.data)
+            return HttpResponse(json, status=200)
+        else:
+            data = {
+                'email': resp['email'],
+                'first_name': resp['given_name'],
+                'last_name': resp['family_name']
+            }
+            serializer = UserSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            json = JSONRenderer().render(serializer.data)
+            return HttpResponse(json, status=200)
+        
+         
+    def post(self, request):
+        username = request.data.get('username')
+        email = request.data.get('email')
+        last_name = request.data.get('last_name')
+        first_name = request.data.get('first_name')
+        user = User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name)
         refresh = RefreshToken.for_user(user)
         data = {
             'refresh': str(refresh),
