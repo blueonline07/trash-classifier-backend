a7a4065a3c9d363eda7c54fb547d84208ac5b80e
Khang Le Duy <khangleduy@Khangs-MacBook-Air.local>
2024-09-01 02:48:13 +0700

    add user create view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auths.urls')),
+    path('users/', include('profiles.urls')),
 ]
